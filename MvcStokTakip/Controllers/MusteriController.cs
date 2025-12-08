f0c2f26c02b0cbeae06dd6169bc7887c9a7c782e
glseren <glserenkarts@gmail.com>
2025-12-09 01:58:12 +0300

    Add YeniMusteri feature for customer creation
    Added a new `YeniMusteri` action in `MusteriController` to handle
    GET and POST requests for creating new customers. Updated the
    `MvcStokTakip.csproj` file to include the new `YeniMusteri.cshtml`
    view. Created the `YeniMusteri.cshtml` view with a form for
    customer input, including fields for first and last names, and
    integrated it with the shared `_MainLayout.cshtml` layout.

--- MvcStokTakip/Controllers/MusteriController.cs
+++ MvcStokTakip/Controllers/MusteriController.cs
@@ -15,5 +15,17 @@ namespace MvcStokTakip.Controllers
             var musteriler = db.TBLMUSTERILER.ToList();
             return View(musteriler);
         }
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER musteriler)
+        {
+            db.TBLMUSTERILER.Add(musteriler);
+            db.SaveChanges();
+            return View();
+        }
     }
 }
